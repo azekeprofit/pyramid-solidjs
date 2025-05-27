ddd62fbd8c62b8ab32febfb4f4a38ef2ca379917
azeke <azekeprofit@gmail.com>
2025-05-27 19:35:40 +0500

    pyramid!

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,20 @@
-import { render } from "solid-js/web";
+import { For, render } from "solid-js/web";
 
-function App(){
-    return <div class="text-green-700 ">trntirtrt</div>;
+const board = [
+    [1],
+    [2, 3],
+    [4, 5, 6],
+    [7, 8, 9, 10]];
+
+function App() {
+    return <div class="block">
+        <For each={board}>{
+            (row, i) => <div class="text-zero text-center">
+                <For each={row}>{(cell, j) =>
+                    <div class='bg-[silver] w-s m-m h-h inline-block cursor-pointer mb-mb hex font-bold font-[Georgia] text-f'>
+                        {cell}
+                    </div>}</For>
+            </div>
+        }</For></div>;
 }
 render(() => <App />, document.body!);
